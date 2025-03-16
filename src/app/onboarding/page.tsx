104f5e22d840c8e3c015ea4e288807984dc84db8
nikolai <niko.mak26@gmail.com>
2025-03-16 14:57:58 +0530

    removed commented code

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -2,18 +2,8 @@
 
 import FormOnboarding from "@/features/onboarding/components/Onboarding";
 
-// import { useState } from 'react';
-// import BasicInfo from '@/features/onboarding/components/BasicInfo';
-// import WorkHistory from '@/features/onboarding/components/WorkHistory'
-// import Education from '@/features/onboarding/components/Education';
-// import OnboardingProgress from '@/features/onboarding/components/OnboardingProgress';
 
 export default function Onboarding() {
-  // const [step, setStep] = useState(0);
-  // const steps = ['Basic Info', 'Work History', 'Education'];
-
-  // const nextStep = () => setStep((prev) => Math.min(prev + 1, steps.length - 1));
-  // const prevStep = () => setStep((prev) => Math.max(prev - 1, 0));
 
   return (
     <div className="min-h-screen bg-gray-50 pt-16">
@@ -26,16 +16,6 @@ export default function Onboarding() {
               Let&apos;s get started by setting up your professional profile
             </p>
           </div>
-
-          {/* Progress Bar */}
-          {/* <OnboardingProgress currentStep={step} steps={steps} /> */}
-
-          {/* Form Steps */}
-          {/* <div className="mt-8">
-            {step === 0 && <BasicInfo onNext={nextStep} />}
-            {step === 1 && <WorkHistory onNext={nextStep} onBack={prevStep} />}
-            {step === 2 && <Education onNext={nextStep} onBack={prevStep} />}
-          </div> */}
 
           <FormOnboarding />
         </div>
