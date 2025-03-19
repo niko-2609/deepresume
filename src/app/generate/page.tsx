6fbc1fb9eb945daebd8b5b1ddfef5cd9408d5124
nikolai <niko.mak26@gmail.com>
2025-03-19 18:46:43 +0530

    added workbench screen

---
+++ src/app/generate/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ResumeGenerator from "@/features/workbench/components/generator";
+
+function GeneratePage() {
+  return (
+    <ResumeGenerator />
+  )
+}
+
+export default GeneratePage;
